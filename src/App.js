8bcdea3b1226c2a81f6ebced8934dfa81ad6954d
Nevena Kuzeljevic <nkuzeljevic8818rn@raf.rs>
2022-04-04 22:24:19 +0200

    Finished employee preview page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DemoPage from "./pages/DemoPage/DemoPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import EmployeePreview from "./pages/EmployeePreviewPage/EmployeePreviewPage";
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" exact element={<DemoPage />} />
-				<Route path="/login" exact element={<LoginPage />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<DemoPage />} />
+        <Route path="/login" exact element={<LoginPage />} />
+        <Route
+          path="/admin/employee-preview"
+          exact
+          element={<EmployeePreview />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
