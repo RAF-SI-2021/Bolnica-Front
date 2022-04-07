37c728377e1a81ba10a51797f7f103e4706d924f
Andrijana Kulezic <andrijana.kulezic@gmail.com>
2022-04-07 23:31:48 +0200

    dropdown and doctors redux - schedule appointment page

--- src/pages/ScheduleAppointmentPage/ScheduleAppointmentPage.js
+++ src/pages/ScheduleAppointmentPage/ScheduleAppointmentPage.js
@@ -1,9 +1,20 @@
-import React from "react";
-import {FaChartPie, FaUser, FaWheelchair} from "react-icons/fa";
+import React, { useEffect } from "react";
+import { FaChartPie, FaUser, FaWheelchair } from "react-icons/fa";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import CustomCalendar from "../../components/CustomCalendar/CustomCalendar";
+import { Dropdown } from "react-bootstrap";
+import "./styles.css";
+import { useDispatch, useSelector } from "react-redux";
+import { getDoctors } from "../../redux/actions/doctors";
 
 const ScheduleAppointmentPage = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getDoctors());
+    }, []);
+
+    const doctors = useSelector((state) => state.doctors);
+
     const links = [
         {
             id: 1,
@@ -27,17 +38,45 @@ const ScheduleAppointmentPage = () => {
         },
     ];
 
+    const getDoctorAppointments = (id) => {
+        console.log(id);
+    };
+
+    // if (doctors) getDoctorAppointments(doctors[0].id);
 
     return (
         <div>
             <div>
                 <Sidebar links={links} />
             </div>
-            <div style={{ marginLeft: "15%", height: "100vh"}}>
-                <CustomCalendar/>
+            <Dropdown className="dropdownButton">
+                <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                    Dr. Paun
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    {/* {doctors.map((doctor) => {
+						return (
+							<Dropdown.Item
+								onClick={() => getDoctorAppointments(doctor.id)}
+							>
+								Dr. {doctor.name}
+							</Dropdown.Item>
+						);
+					})} */}
+                    <Dropdown.Item onClick={() => getDoctorAppointments(1)}>
+                        Dr. Prvi
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={() => getDoctorAppointments(2)}>
+                        Dr. Drugi
+                    </Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+            <div style={{ marginLeft: "15%", height: "100vh" }}>
+                <CustomCalendar />
             </div>
         </div>
     );
-}
+};
 
 export default ScheduleAppointmentPage;
