00bfce6ef1acec2364a46a5f91941b373f6c7a79
Dimitrije <dsejat@gmail.com>
2022-04-08 20:33:49 +0200

    Foundation

--- src/App.js
+++ src/App.js
@@ -4,22 +4,28 @@ import DemoPage from "./pages/DemoPage/DemoPage";
 import DoctorHomepage from "./pages/DoctorHomepage/DoctorHomepage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import EmployeePreview from "./pages/EmployeePreviewPage/EmployeePreviewPage";
+import PatientExaminationPage from "./pages/PatientExaminationPage/PatientExaminationPage";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" exact element={<DoctorHomepage />} />
-        <Route path="/login" exact element={<LoginPage />} />
-        <Route path="/demo-page" exact element={<DemoPage />} />
-        <Route
-          path="/admin/employee-preview"
-          exact
-          element={<EmployeePreview />}
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" exact element={<DoctorHomepage />} />
+				<Route path="/login" exact element={<LoginPage />} />
+				<Route path="/demo-page" exact element={<DemoPage />} />
+				<Route
+					path="/admin/employee-preview"
+					exact
+					element={<EmployeePreview />}
+				/>
+				<Route
+					path="/examination/:id"
+					exact
+					element={<PatientExaminationPage />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
---
+++ src/pages/PatientExaminationPage/PatientExaminationPage.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Header from "../../components/Header/Header";
+import { format } from "date-fns";
+import { FaChartPie, FaWheelchair, FaUser } from "react-icons/fa";
+import { useLocation } from "react-router";
+import { Button } from "react-bootstrap";
+
+const PatientExaminationPage = () => {
+	const location = useLocation();
+	const [patientId, setPatientId] = useState();
+
+	useEffect(() => {
+		const pathParts = location.pathname.split("/");
+		setPatientId(pathParts[pathParts.length - 1]);
+	}, [location]);
+
+	if (patientId) console.log(patientId);
+
+	const links = [
+		{
+			id: 1,
+			text: "Pocetna",
+			path: "/",
+			icon: <FaChartPie />,
+		},
+		{
+			id: 2,
+			text: "Pacijenti",
+			path: "/pacijenti",
+			icon: <FaWheelchair />,
+			dividerAfter: true,
+		},
+		{
+			id: 3,
+			text: "Profil",
+			path: "/profil",
+			icon: <FaUser />,
+		},
+	];
+
+	return (
+		<>
+			<div className="sidebar-link-container">
+				<Sidebar links={links} />
+			</div>
+			<div style={{ marginLeft: "15%" }}>
+				<Header
+					avatarUrl={"nikolaSlika 1.jpg"}
+					welcomeMsg={"Dobro jutro"}
+					userName={"Dr. Paun"}
+					userTitle={"Kardiolog"}
+					day={format(new Date(), "d")}
+					date={format(new Date(), "d MMMM, yyyy")}
+				/>
+			</div>
+		</>
+	);
+};
+
+export default PatientExaminationPage;
