0fc4874534ad0fd604c6b658e630e0fbcc4448c9
dsejat <dsejat@girddynamics.com>
2022-04-10 11:05:23 +0200

    Medical record setup

--- src/components/MedicalRecord/MedicalRecord.js
+++ src/components/MedicalRecord/MedicalRecord.js
@@ -1,34 +1,53 @@
 import React from "react";
 
 const MedicalRecord = () => {
-	const dummyDate = new Date();
-	const stringDate = dummyDate.toLocaleDateString();
+  const dummyDate = new Date();
+  const stringDate = dummyDate.toLocaleDateString();
 
-	const alergies = ["polen", "macja dlaka", "mleko"];
-	const activeDiagnosis = ["slomljena kost", "upala pluca"];
+  const alergies = ["polen", "macja dlaka", "mleko"];
+  const vaccines = ["covid19", "tetanus"];
 
-	return (
-		<div>
-			<div className="patient-info-custom">
-				<div className="patient-personal-info">
-					<p className="patient-info-text">Ime: Pacijent</p>
-					<p className="patient-info-text">Prezime: Pacijentic</p>
-					<p className="patient-info-text">
-						Datum rodjena: {stringDate}
-					</p>
-				</div>
-				<div className="patient-other-info">
-					<p className="patient-info-text">
-						Alergije: {alergies.map((alergy) => `${alergy} `)}
-					</p>
-					<p className="patient-info-text">
-						Aktivne dijagnoze:{" "}
-						{activeDiagnosis.map((diagnosis) => `${diagnosis} `)}
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <>
+      <p className="form-section-heading">Osnovni zdravsteni podaci</p>
+      <div>
+        <div className="patient-info-custom">
+          <div className="patient-personal-info">
+            <p className="patient-info-text">Ime: Pacijent</p>
+            <p className="patient-info-text">Prezime: Pacijentic</p>
+            <p className="patient-info-text">Datum rodjena: {stringDate}</p>
+            <p className="patient-info-text">
+              Alergije: {alergies.map((alergy) => `${alergy} `)}
+            </p>
+          </div>
+          <div className="patient-other-info">
+            <p className="patient-info-text">Krvna grupa: AB</p>
+            <p className="patient-info-text">RH faktor: +</p>
+            <p className="patient-info-text">
+              Vakcine: {vaccines.map((vaccine) => `${vaccine} `)}
+            </p>
+          </div>
+        </div>
+      </div>
+      <p className="form-section-heading">Istorija lekarskih pregleda</p>
+      <div>
+        <div className="patient-info-custom">
+          <div className="patient-personal-info">
+            <p className="patient-info-text">Ime: Pacijent</p>
+            <p className="patient-info-text">Prezime: Pacijentic</p>
+            <p className="patient-info-text">Datum rodjena: {stringDate}</p>
+          </div>
+          <div className="patient-other-info">
+            <p className="patient-info-text">Krvna grupa: AB</p>
+            <p className="patient-info-text">RH faktor: +</p>
+            <p className="patient-info-text">
+              Alergije: {alergies.map((alergy) => `${alergy} `)}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MedicalRecord;
