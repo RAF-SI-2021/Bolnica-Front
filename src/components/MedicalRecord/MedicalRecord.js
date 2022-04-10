6e9ab7b41646fe0330ea61c2b0969b613054a838
dsejat <dsejat@girddynamics.com>
2022-04-10 11:24:28 +0200

    Examination

--- src/components/MedicalRecord/MedicalRecord.js
+++ src/components/MedicalRecord/MedicalRecord.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Table from "../Table/Table";
 
 const MedicalRecord = () => {
   const dummyDate = new Date();
@@ -6,6 +7,26 @@ const MedicalRecord = () => {
 
   const alergies = ["polen", "macja dlaka", "mleko"];
   const vaccines = ["covid19", "tetanus"];
+
+  const headers = ["Datum", "Objektivni nalaz"];
+  const tableContent = [
+    {
+      dateToShow: new Date().toLocaleString(),
+      objectiveFinding: "Objektivni nalaz doktora",
+    },
+    {
+      dateToShow: new Date().toLocaleString(),
+      objectiveFinding: "Objektivni nalaz doktora",
+    },
+    {
+      dateToShow: new Date().toLocaleString(),
+      objectiveFinding: "Objektivni nalaz doktora",
+    },
+  ];
+
+  const handleClick = () => {
+    console.log("Cao");
+  };
 
   return (
     <>
@@ -30,22 +51,11 @@ const MedicalRecord = () => {
         </div>
       </div>
       <p className="form-section-heading">Istorija lekarskih pregleda</p>
-      <div>
-        <div className="patient-info-custom">
-          <div className="patient-personal-info">
-            <p className="patient-info-text">Ime: Pacijent</p>
-            <p className="patient-info-text">Prezime: Pacijentic</p>
-            <p className="patient-info-text">Datum rodjena: {stringDate}</p>
-          </div>
-          <div className="patient-other-info">
-            <p className="patient-info-text">Krvna grupa: AB</p>
-            <p className="patient-info-text">RH faktor: +</p>
-            <p className="patient-info-text">
-              Alergije: {alergies.map((alergy) => `${alergy} `)}
-            </p>
-          </div>
-        </div>
-      </div>
+      <Table
+        headers={headers}
+        tableContent={tableContent}
+        handleClick={handleClick}
+      />
     </>
   );
 };
